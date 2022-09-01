0ec06c390011614c81128e75ab83a6b02b024ab2
Nik Bailey <bail0301@morris.umn.edu>
2022-09-01 13:14:34 -0500

    Starting code for the lab
    Started lab, need to implement quicksort

---
+++ TestInteger.java
@@ -0,0 +1,14 @@
+public class TestInteger implement comparable<Test Integer> {
+    private int value;
+    static public counter;
+
+    public int compareTo(TestInteger other) {
+
+    }
+
+    
+
+    public static void main(String[] args) {
+
+    }
+}
