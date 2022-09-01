63be52017b47a981f455bb5c211bd29a57c1561a
Nik Bailey <bail0301@morris.umn.edu>
2022-09-01 13:24:52 -0500

    Added quicksort
    Added quicksort class for the quicksort nethod.

--- TestInteger.java
+++ TestInteger.java
@@ -8,7 +8,13 @@ public class TestInteger implement comparable<Test Integer> {
 
     
 
+    // Driver Code
     public static void main(String[] args) {
-
+        int[] arr = { 10, 7, 8, 9, 1, 5 };
+        int n = arr.length;
+      
+        quickSort(arr, 0, n - 1);
+        System.out.println("Sorted array: ");
+        printArray(arr, n);
     }
 }
