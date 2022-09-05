5f5517f16eebb8a1c7bf73e915dd51abeb2fc77e
Nik Bailey <bail0301@morris.umn.edu>
2022-09-05 14:59:03 -0500

    Setting up
    Added several helper methods such as a constructor, get methods, set methods. Implemented the compareTo method and added comments as well

--- TestInteger.java
+++ TestInteger.java
@@ -1,15 +1,31 @@
-public class TestInteger implements Comparable<TestInteger> {
-    private int value;
-    static public int counter;
+// Ryan Sajulga and Nik Bailey
 
+public class TestInteger implements Comparable<TestInteger> {
+    private int value; //Variable for setting the value of a TestInteger
+    static public int counter; //Counter for # of comparisons
+
+    //Constructor for TestInteger object
     public TestInteger(int x) {
         value = x;
     }
 
-    public int compareTo(TestInteger other) {
-        counter++;
+    //Setter method to reset the counter
+    public static void resetCounter() {
+        counter = 0;
     }
 
+    //Getter method to get current value of the counter
+    public static int getCounter() {
+        return counter;
+    }
+
+    //compareTo method that increments the counter
+    public int compareTo(TestInteger other) {
+        counter++;
+        return this.compareTo(other);
+    }
+
+    //Creates an ordered array of length size
     public TestInteger[] orderedArray(int size) {
         TestInteger[] arr = new TestInteger[size];
         for(int i = 0; i < size; i++) {
@@ -18,6 +34,7 @@ public class TestInteger implements Comparable<TestInteger> {
         return arr;
     }
 
+    //Creates a random array of length size
     public TestInteger[] randomArray(int size) {
         TestInteger[] arr = new TestInteger[size];
         for(int i = 0; i < size; i++) {
@@ -26,16 +43,24 @@ public class TestInteger implements Comparable<TestInteger> {
         return arr;
     }
 
+    public boolean isSorted(TestInteger[] arr) {
+        for(int i = 1; i < arr.length; i++) {
+            
+        }
+        return true;
+    }
+
     // Driver Code
     public static void main(String[] args) {
+        resetCounter();
         int[] arr1 = new int[20];
         int[] arr2 = new int[20];
 
-        System.out.println("Sorted array: ");
-        Quicksort.printArray(arr1, arr1.length);
+        // System.out.println("Sorted array: ");
+        // Quicksort.printArray(arr1, arr1.length);
 
-        Quicksort.quickSort(arr2, 0, arr2.length - 1);
-        System.out.println("Sorted array: ");
-        Quicksort.printArray(arr2, arr2.length);
+        // Quicksort.quickSort(arr2, 0, arr2.length - 1);
+        // System.out.println("Sorted array: ");
+        // Quicksort.printArray(arr2, arr2.length);
     }
 }
